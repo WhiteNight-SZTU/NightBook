7ab8e662329d297d283b4d43f12c876256988ab5
White Night <1602699904@qq.com>
2023-09-26 10:30:04 +0800

    day 2

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
-<template>
+<template #suffix>
   <div class="login" :class="{'show': show}">
     <h2>登录</h2>
     <form @submit.prevent="login" class="login-form">
       <div class="form-group">
-        <label for="username">用户名</label>
+        <label for="username" style="color: aliceblue;">用户名</label>
         <input type="account" id="username" v-model="username" required />
       </div>
       <div class="form-group">
-        <label for="password">&ensp;密码&ensp;</label>
-        <input type="password" id="password" v-model="password" required />
+        <label for="password" style="color: aliceblue;">密码</label>
+        <div class="password-input">
+          <input
+            :type="showPassword ? 'text' : 'password'"
+            id="password"
+            v-model="password"
+            required
+          />
+          <span @click="togglePasswordVisibility" class="show-password">
+            {{ showPassword ? '隐藏密码' : '显示密码' }}
+            <span class="clickable-span">{{ show_password_emoji }}</span>
+          </span>
+        </div>
+     
       </div>
       <button type="submit">登录</button>
     </form>
@@ -22,10 +34,22 @@ export default {
       username: "",
       password: "",
       show: false,
+      showPassword:false,
+      show_password_emoji:"🙈",
     };
   },
   methods: {
     login() {
+    },
+    togglePasswordVisibility() {
+      this.showPassword = !this.showPassword;
+      if(this.show_password_emoji=="🙈")
+      {
+        this.show_password_emoji = "🐵";
+      }else
+      {
+        this.show_password_emoji = "🙈";
+      }
     },
   },
   mounted() {
@@ -44,8 +68,8 @@ export default {
   flex-direction: column; 
   align-items: center; 
   justify-content: center; 
-  min-width: 40vh;
-  min-height: 40vh;
+  min-width: 50vh;
+  min-height: 50vh;
   position: fixed;
   top: 50%;
   left: 50%;
@@ -65,9 +89,18 @@ export default {
 }
 .login input[type="account"],
 .login input[type="password"],
-.login button {
+.password-input input[type="text"],
+.login button{
   width: 100%;
   padding: 10px;
   margin-bottom: 10px;
+}
+
+.login button{
+  margin-top: 15px;
+}
+.show-password{
+  user-select: none;
+  cursor: pointer;
 }
 </style>
