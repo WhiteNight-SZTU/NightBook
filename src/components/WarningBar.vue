ffa4f5641c05eafbe030c2abf5f1369e254da0f7
White Night <1602699904@qq.com>
2023-10-06 23:39:32 +0800

    Completed WarningBar

--- src/components/WarningBar.vue
+++ src/components/WarningBar.vue
@@ -1,6 +1,54 @@
 <template>
 	<section>
-		<div class="scroll">
+		<div class="scroll" style="--d:25;--y:200">
+			<div>
+				<span>
+					网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 
+				</span>
+			</div>
+			<div>
+				<span>
+					网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 
+				</span>
+			</div>
+		</div>
+		<div class="scroll" style="--d:-25;--y:400">
+			<div>
+				<span>
+					网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 
+				</span>
+			</div>
+			<div>
+				<span>
+					网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 
+				</span>
+			</div>
+		</div>
+		<div class="scroll" style="--d:15;--y:500">
+			<div>
+				<span>
+					网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 
+				</span>
+			</div>
+			<div>
+				<span>
+					网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 
+				</span>
+			</div>
+		</div>
+		<div class="scroll" style="--d:-5;--y:50">
+			<div>
+				<span>
+					网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 
+				</span>
+			</div>
+			<div>
+				<span>
+					网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 
+				</span>
+			</div>
+		</div>
+		<div class="scroll" style="--d:-15;--y:-350">
 			<div>
 				<span>
 					网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 网站维护中⚠ 
@@ -15,5 +63,5 @@
 	</section>
 </template>
 
-<style src="/src/assets/css/style.css">
+<style src="/src/assets/css/WarningBarStyle.css">
 </style>
