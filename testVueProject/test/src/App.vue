1a6a399407229342ecc24d985902edbed01a7bd2
White Night <1602699904@qq.com>
2023-10-09 22:45:11 +0800

    Added test vue project

---
+++ testVueProject/test/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {ref} from 'vue'
+const flag=ref(true)
+
+function toggle(){
+  if(flag.value==true)
+  {
+    flag.value=false;
+  }else if(flag.value==false)
+  {
+    flag.value=true;
+  }
+}
+</script>
+
+<template>
+<button @click="toggle">click this</button>
+<span v-if="flag">State:True</span>
+<span v-else>State:False</span>
+</template>
